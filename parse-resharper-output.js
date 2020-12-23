cf1b31317ddc99bc27d865ce6fafe415133695cf
Vladimir Tikhonov <v.tikhonov@lunalabs.uk>
2020-12-23 17:01:10 +0300

    Add pathPrefix opt and exclude shader errors

--- parse-resharper-output.js
+++ parse-resharper-output.js
@@ -1,7 +1,7 @@
 const fs = require( 'fs' );
 const xml2js = require( 'xml2js' );
 
-async function analyzeReport( reportText, basePath ) {
+async function analyzeReport( reportText, basePath, pathPrefix ) {
     const reportJson = await xml2js.parseStringPromise( reportText );
     const issuesArr = reportJson.Report.Issues[ 0 ];
     if ( !issuesArr || issuesArr.length === 0 ) {
@@ -13,7 +13,7 @@ async function analyzeReport( reportText, basePath ) {
     issues.forEach( ( i ) => {
         i.File = i.File.split( '\\' ).join( '/' );
     } );
-    issues = issues.filter( ( i ) => i.File.startsWith( basePath ) && i.TypeId !== 'CSharpErrors' );
+    issues = issues.filter( ( i ) => i.File.startsWith( basePath ) && i.TypeId !== 'CSharpErrors' && i.TypeId !== 'CppCompilerErrors' );
     issues.forEach( ( i ) => {
         i.File = i.File.replace( basePath, '' );
     } );
@@ -24,17 +24,17 @@ async function analyzeReport( reportText, basePath ) {
 
     const reportedIssues = new Set();
     for ( const issue of issues ) {
-        const issueText = `::error file=${issue.File},line=${issue.Line},col=${issue.Offset}::${issue.Message}`;
+        const issueText = `::error file=${pathPrefix + issue.File},line=${issue.Line},col=${issue.Offset}::${issue.Message}`;
         if ( reportedIssues.has( issueText ) ) {
             continue;
         }
         reportedIssues.add( issueText );
-        console.log( `${issue.File}:${issue.Line}:` );
+        console.log( `${pathPrefix + issue.File}:${issue.Line}:` );
         console.log( issueText );
     }
     process.exit( 1 );
 }
 
-const [ _, __, reportFileName, basePath ] = process.argv;
+const [ _, __, reportFileName, basePath, pathPrefix = '' ] = process.argv;
 const reportFileText = fs.readFileSync( reportFileName ).toString( 'utf-8' );
-analyzeReport( reportFileText, basePath );
+analyzeReport( reportFileText, basePath, pathPrefix );
